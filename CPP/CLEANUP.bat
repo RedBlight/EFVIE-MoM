4ca4c3e5fa24fac1d7c20dedcfb99b1a08aad13d
MAHMUT AKKUŞ <yepisyenilmis@gmail.com>
2018-02-01 01:24:56 +0300

    Added a cleanup procedure for VS.

---
+++ CPP/CLEANUP.bat
@@ -0,0 +1,36 @@
+@RD /S /Q ".vs"
+
+@RD /S /Q "AllFiles\CPU"
+@RD /S /Q "AllFiles\CUDA"
+@RD /S /Q "AllFiles\OpenCL"
+@RD /S /Q "AllFiles\Unified"
+
+@RD /S /Q "Electrify\CPU"
+@RD /S /Q "Electrify\CUDA"
+@RD /S /Q "Electrify\OpenCL"
+@RD /S /Q "Electrify\Unified"
+
+@RD /S /Q "MakeFace\CPU"
+@RD /S /Q "MakeFace\CUDA"
+@RD /S /Q "MakeFace\OpenCL"
+@RD /S /Q "MakeFace\Unified"
+
+@RD /S /Q "MakeMesh\CPU"
+@RD /S /Q "MakeMesh\CUDA"
+@RD /S /Q "MakeMesh\OpenCL"
+@RD /S /Q "MakeMesh\Unified"
+
+@RD /S /Q "MakeMoM\CPU"
+@RD /S /Q "MakeMoM\CUDA"
+@RD /S /Q "MakeMoM\OpenCL"
+@RD /S /Q "MakeMoM\Unified"
+
+@RD /S /Q "MakeQuadrature\CPU"
+@RD /S /Q "MakeQuadrature\CUDA"
+@RD /S /Q "MakeQuadrature\OpenCL"
+@RD /S /Q "MakeQuadrature\Unified"
+
+@RD /S /Q "New EFVIE-MOM Project\CPU"
+@RD /S /Q "New EFVIE-MOM Project\CUDA"
+@RD /S /Q "New EFVIE-MOM Project\OpenCL"
+@RD /S /Q "New EFVIE-MOM Project\Unified"
